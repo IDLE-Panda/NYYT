59abeccae6b24b07f2d3aa1dfea7d57470dbb120
IDLE-Panda <cliuxy@gmail.com>
2020-07-27 15:53:58 -0400

    update while loop

--- while_loop.py
+++ while_loop.py
@@ -1,13 +1,13 @@
-# i = 1
-# while i <= 5:
-#     print("*"*i)
-#     i += 1
+i = 1
+while i <= 5:
+    print("*"*i)
+    i += 1
 
-# i = 20
-# while i >= 0:
-#     if i % 2 == 0:
-#         print(i)
-#     i -= 1
+i = 20
+while i >= 0:
+    if i % 2 == 0:
+        print(i)
+    i -= 1
 
 nums = []
 sum = 0
